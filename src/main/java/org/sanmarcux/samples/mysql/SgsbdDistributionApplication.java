b21ebad936ac8a43348d9402e2c882ac5255986a
César Díaz <cesar.dl88@gmail.com>
2023-08-18 11:01:13 -0500

    Initial commit

---
+++ src/main/java/org/sanmarcux/samples/mysql/SgsbdDistributionApplication.java
@@ -0,0 +1,13 @@
+package org.sanmarcux.samples.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgsbdDistributionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgsbdDistributionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sanmarcux/samples/mysql/sgsbddistribution/SgsbdDistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sanmarcux.samples.mysql.sgsbddistribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgsbdDistributionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
